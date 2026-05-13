59c351fc197d3b6a1482ec2eb9c23dbfc83ad736
radenpioneer <bohrium.bearded_64@icloud.com>
2026-05-13 18:01:06 +0700

    fix: use absolute alias for TrainingDetailView to avoid relative path errors

--- src/app/(dashboard)/dashboard/trainings/[branch]/[year]/[identifier]/page.tsx
+++ src/app/(dashboard)/dashboard/trainings/[branch]/[year]/[identifier]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation'
 import { trainingQuery } from '~/db/query/training'
-import { TrainingDetailView } from '../../../../_components/training-detail-view'
+import { TrainingDetailView } from '~/app/(dashboard)/dashboard/trainings/_components/training-detail-view'
 
 interface PageProps {
   params: Promise<{
